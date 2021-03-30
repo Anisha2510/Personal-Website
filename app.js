69ad37920daba9dffc2e0c8d3d19478b17a843b2
Anisha <anishaanil1025@gmail.com>
2021-03-30 12:32:25 +0530

    Add changes to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-
+app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(__dirname + '/public'));
@@ -12,7 +12,9 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-
-app.listen(3000, () =>{
-    console.log('PORT 3000 CONNECTED!');
-})
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+});
+// app.listen(3000, () =>{
+//     console.log('PORT 3000 CONNECTED!');
+// })
